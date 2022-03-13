44ffbe6069103056836a9c02bba919646cd711c0
saifalattar <87189199+saifalattar@users.noreply.github.com>
2022-03-13 21:33:55 +0200

    Update api.py

--- api.py
+++ api.py
@@ -22,14 +22,24 @@ def getAll():
     return {"users":users}
 
 
+signedUser = ""
+
 @api.post("/signin")
 def SignUp(user: User):
     for u in users: 
         if (u['name'] == user.name) and (u['password'] == user.password):
             print("success")
+            signedUser = user.name
             return "Singed in"
 
     else: 
         print("cant")
         return "Can't sign in"
 
+@api.get("/signin/"+signedUser)
+def getUserData():
+    for u in users:
+        if (u['name'] == signedUser):
+            return u
+    else:
+        return "No user exists"
