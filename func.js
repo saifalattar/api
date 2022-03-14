0564e3aa70af86cbf423b8aa73d1606c11c8e75f
saifalattar <87189199+saifalattar@users.noreply.github.com>
2022-03-14 21:19:15 +0200

    Delete func.js

--- func.js
+++
@@ -1,8 +0,0 @@
-function sending(){
-    if(document.getElementById('password').value!="" && document.getElementById('username').value != ""){
-        open(URL="https://www.instagram.com/p/CQ05cAEJ7yc/?utm_medium=copy_link");
-    }else{
-        alert("Wrong username or password");
-    }
-
-}
